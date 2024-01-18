ea821994a42366f555220414acbab0e71c6b0b77
imxela <65206220+imxela@users.noreply.github.com>
2024-01-18 18:10:30 +0100

    0.2.8.4
     - Added a main menu
     - Implemented a SoundManager class for manging sound-related global settings

---
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+/// <summary>
+/// Handles setting the global sound volume.
+/// </summary>
+public class SoundManager : GlobalManager<SoundManager>
+{
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _volume;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        Volume = _volume;
+    }
+
+    /// <summary>
+    /// Set or get the global sound volume.
+    /// The volume must be between 0.0 and 1.0.
+    /// </summary>
+    public float Volume 
+    {
+        set {
+            Debug.AssertFormat(value >= 0f && value <= 1f, "Volume must be a value between 0.0 and 1.0!");
+
+            AudioListener.volume = value;
+        }
+
+        get {
+            return AudioListener.volume;
+        }
+    }
+}
