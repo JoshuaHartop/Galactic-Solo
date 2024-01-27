deb37366b23b22fc4da335968f6d2a30e425efb4
JoshuaHartop <119062032+JoshuaHartop@users.noreply.github.com>
2024-01-27 07:36:51 -0400

    0.6.21.2
    - added saving to points
    - added loading points

--- Assets/Scripts/UI/Points Script.cs
+++ Assets/Scripts/UI/Points Script.cs
@@ -9,9 +9,34 @@ public class PointsScript : LocalManager<PointsScript>
     protected override bool Persistent => false;
     public TMP_Text text;
     private int _points;
+    private PointsData _pointsData;
+
+    public void Awake()
+    {
+        _pointsData = PointsData.Load();
+        _points = _pointsData._savePoints;
+    }
     public void addPoints(int pointsToAdd)
     {
         _points += pointsToAdd;
         text.text = "Points: " + _points.ToString();
+        _pointsData._savePoints = _points;
     }
+
+    public void removePoints(int pointsToRemove)
+    {
+        _points -= pointsToRemove;
+        text.text = "Points: " + _points.ToString();
+        _pointsData._savePoints = _points;
+    }
+
+    public void OnDestroy()
+    {
+        _pointsData.Save();
+    }
+}
+
+public class PointsData : SaveData<PointsData>
+{
+    public int _savePoints;
 }
